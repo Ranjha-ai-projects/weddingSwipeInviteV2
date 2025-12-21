6e129fbcf35854645fde3595d079f5160d7a2f99
Ranjha-ai-projects <ranjhaaiprojects@gmail.com>
2025-12-21 10:31:40 +0530

    updated images

--- data.ts
+++ data.ts
@@ -12,28 +12,28 @@ export const cardsData: CardData[] = [
     id: 1,
     title: "Where it All Began",
     description: "Our first date was just a simple coffee, but we knew from that first conversation that this was something special.",
-    image: "https://imgs.search.brave.com/KrnojTMHVRpYN8Vrg0NvEVAh_BZMlNdoI1WbR72nYxE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA5Lzk0LzI5LzU5/LzM2MF9GXzk5NDI5/NTk4N19TTm40a0VI/bnN3T1FNWlQxcGtM/a3ROcDVLR2haRmt5/TC5qcGc",
+    image: "/images/scene-1.png",
     tag: "The First Date"
   },
   {
     id: 2,
     title: "Better Together",
     description: "From morning runs to weekend adventures, we found a partner who keeps up with every step of the journey.",
-    image: "https://imgs.search.brave.com/prWb1Cwx6NSZj2qUyJHZzU_B1dyOku7jTpHXf8UDy8k/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by95b3VuZy1jb3Vw/bGVzLWpvZ2dpbmct/cGFya18zNTE1MC04/MTMuanBnP3NlbXQ9/YWlzX2luY29taW5n/Jnc9NzQwJnE9ODA",
+    image: "/images/scene-2.png",
     tag: "Our Passions"
   },
   {
     id: 3,
     title: "Life of the Party",
     description: "We've shared countless nights of dancing and laughter. Life is just one big celebration when we're together.",
-    image: "https://imgs.search.brave.com/RSM3XHTwvnbNprRfl2oLFJwdO1hPsaNdSr90KzqH6_w/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi95b3Vu/Zy1jb3VwbGUtZW5q/b3lpbmctcGFydHkt/Y2x1Yi1oYXZpbmct/ZnVuLWRhbmNpbmct/MzM4MzEzNjcuanBn",
+    image: "/images/scene-3.png",
     tag: "Memories"
   },
   {
     id: 4,
     title: "Our Forever Starts",
     description: "Now, we invite you to witness the beginning of our greatest chapter yet. Join us for our wedding celebration!",
-    image: "https://imgs.search.brave.com/ioRg7zbmOtN9GMUACYgPCw4F6xn1dLnNlmvyeLSlXGg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/c3Rld2Fsa2VycGhv/dG9ncmFwaHkuY29t/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDI0/LzEyL0Jlc3QtV2Vk/ZGluZy1QaG90b3Mt/MjAyNS1TdGVwaGVu/LVdhbGtlcjE4Ny04/MDB4MTIwMC5qcGc",
+    image: "/images/scene-4.png",
     tag: "The Big Day"
   }
 ];
